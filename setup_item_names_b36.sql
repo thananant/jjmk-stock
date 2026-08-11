e684d659d03bf39985dd2db7c5925754ef6af673
thananant <thananantpatt@gmail.com>
2026-08-11 14:38:30 +0700

    Delete setup_item_names_b36.sql

--- setup_item_names_b36.sql
+++
@@ -1,4 +0,0 @@
--- JJMK b36: bill item <-> stock item name link (run once, safe to re-run)
-alter table supplier_items add column if not exists stock_name text;
-select 'stock_name column' as ok, count(*) from information_schema.columns
-where table_name='supplier_items' and column_name='stock_name';
